66f0d063d3db52e4982035fa10a6f2175181b6e2
George Tian <me@georgetian.com>
2024-10-13 00:07:49 +1100

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,28 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Jason Li Portfolio",
-  description: "Jason Li Portfolio",
+  title: 'Jason Li Portfolio',
+  description: 'Jason Li Portfolio',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
+  return <html lang='en'>
+    <body
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
+      {children}
+    </body>
+  </html>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,127 +1,173 @@
+// 'use client'
+
+// import { motion, useScroll, useTransform } from 'framer-motion';
+// import Image from 'next/image';
+// import { useEffect, useRef, useState } from 'react';
+// import Lenis from 'lenis'
+
+// const width = 600
+// const height = 400
+
+// function getPlaceholderImageUrl() {
+//   return `https://picsum.photos/seed/${Math.round(Math.random() * 100)}/${width}/${height}`
+// }
+
+// function AnimatedImage({src}: {src: string}) {
+//   const ref = useRef(null);
+//   const { scrollYProgress } = useScroll({
+//     target: ref,
+//     offset: ['end end', 'start start']
+//   });
+//   const [scale, setScale] = useState<number>(1)
+//   // const scaleTransform = useTransform(() => 1 - Math.abs(0.5 - scrollYProgress.get()))
+//   return <motion.div
+//     ref={ref}
+//     initial={{ 
+//       opacity: 0,
+//     }}
+//     whileInView={{
+//       opacity: 1,
+//     }}
+//     transition={{ 
+//       duration: 0.5,
+//     }}
+//     animate={{
+//       width: width * scale,
+//       height: height * scale,
+//     }}
+//     // style={{
+//     //   scaleX: scale,
+//     //   scaleY: scale,
+//     // }}
+//     onHoverStart={() => setScale(1.2)}
+//     onHoverEnd={() => setScale(1)}
+//     className="relative space-x-4 flex items-center border-2 border-red-600"
+//   >
+//     <Image
+//       src={src}
+//       alt=''
+//       fill
+//       objectFit='contain'
+//     />
+//     <div>
+//       text<br/>
+//       text<br/>
+//       text<br/>
+//       text<br/>
+//     </div>
+//   </motion.div>
+// }
+
+
+
+// export default function Home() {
+//   useEffect(() => {
+//     const lenis = new Lenis()
+//     function raf(time: number) {
+//       lenis.raf(time)
+//       requestAnimationFrame(raf)
+//     }
+//     requestAnimationFrame(raf)
+//   })
+//   const srcs = []
+//   const imgs = []
+//   for (let i = 0; i < 10; i++) {
+//     srcs.push(getPlaceholderImageUrl())
+//     imgs.push(<AnimatedImage key={i} src={srcs[srcs.length - 1]}/>)
+//   }
+//   return (
+//     <div className="flex justify-center">
+//       <div className="flex-col justify-center align-center space-y-16 border-2 border-green-600">
+//         {imgs}
+//       </div>
+//     </div>
+//   );
+// }
+
 'use client'
 
-import { motion, useScroll, useTransform } from "framer-motion";
-import Image from "next/image";
-import { useEffect, useRef } from "react";
-import Lenis from 'lenis'
+import { motion, useScroll } from 'framer-motion';
+import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
+import Lenis from 'lenis';
 
-const width = 600
-const height = 400
-const placeholderImageUrl = `https://via.placeholder.com/${width}x${height}.png`
+const width = 600;
+const height = 400;
 
-function AnimatedImage() {
+function getPlaceholderImageUrl() {
+  return `https://picsum.photos/seed/${Math.round(Math.random() * 100)}/${width}/${height}`;
+}
+
+function AnimatedImage({ src }: { src: string }) {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["end end", "start start"]
+    offset: ['end end', 'start start'],
   });
-  const scaleTransform = useTransform(() => 1 - Math.abs(0.5 - scrollYProgress.get()))
-  return <motion.div
-    ref={ref}
-    initial={{ 
-      opacity: 0,
-    }}
-    whileInView={{
-      opacity: 1,
-    }}
-    transition={{ 
-      duration: 0.5,
-    }}
-    style={{
-      position: "relative",
-      // border: '1px solid red',
-      scaleX: scaleTransform,
-      scaleY: scaleTransform,
-      display: 'flex',
-      alignItems: 'center',
-    }}
-  >
-    <Image
-      src={placeholderImageUrl}
-      alt=""
-      width={width}
-      height={height}
-    />
-    <div style={{marginLeft: '16px'}}>
-      text<br/>
-      text<br/>
-      text<br/>
-      text<br/>
-    </div>
-  </motion.div>
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 0.5,
+      }}
+      animate={{
+        width: isHovered ? width * 1.2 : width,
+        height: isHovered ? height * 1.2 : height,
+      }}
+      onHoverStart={() => setIsHovered(true)}
+      onHoverEnd={() => setIsHovered(false)}
+      className={`relative mx-auto`}
+    >
+      <Image
+        src={src}
+        alt=''
+        fill
+        objectFit='contain'
+      />
+      <div className="absolute flex items-center justify-center">
+        text<br />
+        text<br />
+        text<br />
+        text<br />
+      </div>
+    </motion.div>
+  );
 }
 
 export default function Home() {
 
-  // useEffect(() => {
-  //   const lenis = new Lenis()
-  //   function raf(time: number) {
-  //     lenis.raf(time)
-  //     requestAnimationFrame(raf)
-  //   }
-  //   requestAnimationFrame(raf)
-  // })
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
 
-  const imgs = []
+  const srcs = [];
+  const imgs = [];
   for (let i = 0; i < 10; i++) {
-    imgs.push(<AnimatedImage key={i} />)
+    srcs.push(getPlaceholderImageUrl());
+    imgs.push(
+      <AnimatedImage 
+        key={i} 
+        src={srcs[srcs.length - 1]} 
+      />
+    );
   }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {imgs}
-      </main>
+    <div className="mx-auto space-y-16 pd m-64">
+      {imgs}
     </div>
   );
 }
-
-// import "./styles.css";
-// import { useRef } from "react";
-// import { motion, useScroll } from "framer-motion";
-
-// function Item() {
-//   const ref = useRef(null);
-//   const { scrollYProgress } = useScroll({
-//     target: ref,
-//     offset: ["end end", "start start"]
-//   });
-
-//   return (
-//     <section>
-//       <div ref={ref}>
-//         <figure className="progress">
-//           <svg id="progress" width="75" height="75" viewBox="0 0 100 100">
-//             <circle cx="50" cy="50" r="30" pathLength="1" className="bg" />
-//             <motion.circle
-//               cx="50"
-//               cy="50"
-//               r={Math.abs(15 - scrollYProgress.get())}
-//               pathLength="1"
-//               className="indicator"
-//               style={{ pathLength: scrollYProgress }}
-//             />
-//           </svg>
-//         </figure>
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default function App() {
-//   return (
-//     <>
-//       <Item />
-//       <Item />
-//       <Item />
-//       <Item />
-//       <Item />
-//       <Item />
-//       <Item />
-//       <Item />
-//       <Item />
-//       <Item />
-//       <Item />
-//       <Item />
-//     </>
-//   );
-// }
